03818778824b2dbf6660aa1d3bd0982335bcd2f7
IlyaGoncharovY <goncharov.i@rambler.ru>
2024-04-15 00:57:41 +0300

    fix(app) render first arr, second arr, footer

--- src/features/app/App.tsx
+++ src/features/app/App.tsx
@@ -1,10 +1,13 @@
-import './App.css';
+import {FirstNumbersComponent, SecondNumbersComponent} from '../editNumbers';
+import {FooterComponent} from '../footer';
 
 export const App = () => {
 
   return (
-    <>
-
-    </>
+    <div>
+      <FirstNumbersComponent/>
+      <SecondNumbersComponent/>
+      <FooterComponent/>
+    </div>
   );
 };
