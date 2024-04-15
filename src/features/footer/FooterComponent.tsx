efd5bcc8c18ca073f12481440f42e380b1cc8914
IlyaGoncharovY <goncharov.i@rambler.ru>
2024-04-16 00:57:02 +0300

    fix(footer component) get selected numbers

--- src/features/footer/FooterComponent.tsx
+++ src/features/footer/FooterComponent.tsx
@@ -1,9 +1,19 @@
+import {useAppSelector} from '../../store';
+
 import s from './Footer.module.css';
 
 export const FooterComponent = () => {
+  const selectedNumbers = useAppSelector(state =>
+    state.selectedNumbers.selectedNumbers);
+
+  const buttonDisabled = selectedNumbers.length === 9;
+
   return (
     <div className={s.footerContainer}>
-      <button className={s.button}>
+      <button
+        className={s.button}
+        disabled={!buttonDisabled}
+      >
           Показать результат
       </button>
     </div>
