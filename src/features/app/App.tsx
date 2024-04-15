4a8715a8c05e8f67bbc2dd3101b2e4fdd59c1a9d
IlyaGoncharovY <goncharov.i@rambler.ru>
2024-04-15 14:41:47 +0300

    fix(app) refactor style app

--- src/features/app/App.tsx
+++ src/features/app/App.tsx
@@ -1,12 +1,16 @@
 import {NumbersComponent} from '../editNumbers';
 import {FooterComponent} from '../footer';
 
+import s from './App.module.css';
+
 export const App = () => {
 
   return (
-    <div>
-      <NumbersComponent/>
-      <FooterComponent/>
+    <div className={s.appContainer}>
+      <div className={s.lotteryContainer}>
+        <NumbersComponent/>
+        <FooterComponent/>
+      </div>
     </div>
   );
 };
