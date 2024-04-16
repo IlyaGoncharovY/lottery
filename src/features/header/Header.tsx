20e286b0e4953a1ead5b58d29db7e7eed8fed38e
IlyaGoncharovY <goncharov.i@rambler.ru>
2024-04-16 14:07:04 +0300

    fix(Header) added changeNumbersValue reducer function.

--- src/features/header/Header.tsx
+++ src/features/header/Header.tsx
@@ -1,12 +1,22 @@
 import img from '../../assets/img/magic-wand.png';
 
+import {useAppDispatch} from '../../store';
+import {changeNumbersValue} from '../editNumbers/reducer/numberReducer.ts';
+
 import s from './Header.module.css';
 
 export const Header = () => {
+
+  const dispatch = useAppDispatch();
+
+  const handleImgClick = () => {
+    dispatch(changeNumbersValue());
+  };
+
   return (
     <div className={s.headerContainer}>
       <div>Билет 1</div>
-      <img src={img} alt="magic"/>
+      <img src={img} alt="magic" onClick={handleImgClick}/>
     </div>
   );
 };
