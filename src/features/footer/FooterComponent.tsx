74aace35a1f2a39f6b5e61e23eeb76d14dd1270e
IlyaGoncharovY <goncharov.i@rambler.ru>
2024-04-16 21:35:41 +0300

    fix(footer component) added logic for render rules lottery

--- src/features/footer/FooterComponent.tsx
+++ src/features/footer/FooterComponent.tsx
@@ -1,57 +1,73 @@
-import {useEffect, useState} from 'react';
+import {useEffect} from 'react';
 
+import {numberForApp} from '../../common';
 import {useAppDispatch, useAppSelector} from '../../store';
+
 import {skipIsCheckProperty} from '../editNumbers/reducer/numberReducer.ts';
 
+import {useRandomNumbers} from './hook/useRandomNumbers.ts';
+
 import s from './Footer.module.css';
+import {apiSlice} from './api/apiSlice.ts';
+import {clearSelectedNumbersArray, isWonLottery} from './reducer/footerReducer.ts';
 
 export const FooterComponent = () => {
-  const [randomNumbers, setRandomNumbers] = useState<number[]>([]);
+  const {selectedNumbers, isWon } = useAppSelector(state =>
+    state.selectedNumbers);
 
-  const selectedNumbers = useAppSelector(state =>
-    state.selectedNumbers.selectedNumbers);
+  const [createTicket,
+    {
+      error: createTicketError,
+      isLoading: createTicketLoading,
+    },
+  ] = apiSlice.useSendResultTicketMutation();
+
+  const {randomNumbers, generateRandomNumbers} = useRandomNumbers();
 
   const dispatch = useAppDispatch();
-
-  const generateRandomNumbers = () => {
-    const randomNumbersSet = new Set<number>();
-    while (randomNumbersSet.size < 21) {
-      randomNumbersSet.add(Math.floor(Math.random() * 90) + 1);
-    }
-    const randomNumbersArray = Array.from(randomNumbersSet);
-    setRandomNumbers(randomNumbersArray);
-  };
 
   useEffect(() => {
     generateRandomNumbers();
   }, []);
 
-  const handleShowResult = () => {
+  const handleShowResult = async () => {
     const matchingNumbers = selectedNumbers.filter(num => randomNumbers.includes(num.value));
-    console.log(matchingNumbers);
-    if (matchingNumbers.length >= 4) {
-      console.log('Win!');
-    } else {
-      console.log('Lose!');
+    const filteredFirstNumbers = selectedNumbers.filter(num => num.id >= 1 && num.id <= 19);
+    const filteredSecondNumbers = selectedNumbers.filter(num => num.id >= 20 && num.id <= 21);
+    const firstField = filteredFirstNumbers.map(el => el.value);
+    const secondField = filteredSecondNumbers.map(el => el.value);
+    const isTicketWon = matchingNumbers.length >= numberForApp.matchingNumbersLength;
+
+    if (isTicketWon) {
+      dispatch(isWonLottery(true));
     }
 
-    dispatch(skipIsCheckProperty());
-
-    generateRandomNumbers();
+    try {
+      await createTicket({ firstField, secondField, isTicketWon });
+      generateRandomNumbers();
+    } catch (error) {
+      console.error('Error:', error);
+    } finally {
+      dispatch(skipIsCheckProperty());
+      dispatch(clearSelectedNumbersArray());
+    }
   };
 
-  console.log(randomNumbers);
-  const buttonDisabled = selectedNumbers.length === 9;
+  const buttonDisabled = selectedNumbers.length === numberForApp.selectedNumbersLength;
 
   return (
     <div className={s.footerContainer}>
       <button
         className={s.button}
-        disabled={!buttonDisabled}
+        disabled={!buttonDisabled || createTicketLoading}
         onClick={handleShowResult}
       >
-          Показать результат
+        {createTicketLoading ? 'Отправка' : 'Показать результат'}
       </button>
+      <div>
+        {createTicketError && <h1>Ошибка...</h1>}
+        {isWon ? <h1>Поздравляю с победой!!</h1> : <></>}
+      </div>
     </div>
   );
 };
