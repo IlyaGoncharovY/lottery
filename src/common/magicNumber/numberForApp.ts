37eb093a58610ad33758a61049e6e0ec84f3f6a7
IlyaGoncharovY <goncharov.i@rambler.ru>
2024-04-16 21:30:53 +0300

    feat(magic numbers) added numberForApp.ts

---
+++ src/common/magicNumber/numberForApp.ts
@@ -0,0 +1,18 @@
+/**
+ * object for edit number use in app
+ * @const matchingNumbersLength: 4
+ * @const countFirsLength: 8
+ * @const countSecondLength: 1
+ * @const selectedNumbersLength: 9
+ * @const pressedFirsCount: 9
+ * @const pressSecondCount: 1
+ */
+export const numberForApp = {
+  matchingNumbersLength: 4,
+  countFirsLength: 8,
+  countSecondLength: 1,
+  selectedNumbersLength: 9,
+  pressedFirsCount: 9,
+  pressSecondCount: 1,
+};
+
